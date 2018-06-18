9d0d0dbd6a1dba4073c01b6d599db797f1da7a3f
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-18 18:12:57 +0200

    Added all files and program 11_4 working as it should

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function phone(brand, model, price, year, color) {
+	this.brand = brand;
+	this.model = model;
+	this.price = price;
+	this.year = year;
+	this.color = color;
+}
+
+phone.prototype.printInfo = function() {
+	console.log(this.brand + ' ' + this.model + '    price: ' + this.price +'PLN    year of production: ' + this.year + '    color: ' + this.color + '.');
+	document.getElementById("tel").innerHTML = (this.brand + this.model + '    price: ' + this.price +'PLN    year of production: ' + this.year + '    color: ' + this.color + '.');
+}
+
+var SamsungGalaxyS8 = new phone('Samsung', 'GalaxyS8', 3500, 2018, 'black');
+var IphoneX = new phone('Apple', 'IphoneX', 4500, 2018, 'gold');
+
+SamsungGalaxyS8.printInfo();
+
+IphoneX.printInfo();
