20d0ede3f57cb35a5d09f6fe4872552b9b8db6fd
Adam Nelip <Adam.Nelip@gmail.com>
2018-06-19 13:16:56 +0200

    Everything added and working, changed names

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function phone(brand, model, price, year, color) {
+function Phone(brand, model, price, year, color) {
 	this.brand = brand;
 	this.model = model;
 	this.price = price;
@@ -6,14 +6,14 @@ function phone(brand, model, price, year, color) {
 	this.color = color;
 }
 
-phone.prototype.printInfo = function() {
+Phone.prototype.printInfo = function() {
 	console.log(this.brand + ' ' + this.model + ', price: ' + this.price +' PLN, year of production: ' + this.year + ', color: ' + this.color + '.');
 	document.getElementById('tel').innerHTML = this.brand + ' ' + this.model + ', price: ' + this.price +' PLN, year of production: ' + this.year + ', color: ' + this.color + '.';
 }
 
-var SamsungGalaxyS8 = new phone('Samsung', 'GalaxyS8', 3500, 2018, 'black');
-var IphoneX = new phone('Apple', 'IphoneX', 4500, 2018, 'gold');
+var samsungGalaxyS8 = new Phone('Samsung', 'GalaxyS8', 3500, 2018, 'black');
+var iphoneX = new Phone('Apple', 'IphoneX', 4500, 2018, 'gold');
 
-SamsungGalaxyS8.printInfo();
+samsungGalaxyS8.printInfo();
 
-IphoneX.printInfo();
+iphoneX.printInfo();
